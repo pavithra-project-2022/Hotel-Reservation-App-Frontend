f030cc42887e224ab6e7ab1e8d44563976a559d8
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-10 16:43:31 +0800

    commited

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
+  const { dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogOut = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGOUT" });
+    try {
+      dispatch({ type: "LOGOUT", payload: false });
+      navigate("/login");
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">SmartBooking</span>
+        </Link>
+        {user ? (
+          <div className="navItems">
+            <span className="navButton" style={{ color: "inherit" }}>
+              {user.username}{" "}
+            </span>
+            <span
+              className="navButton"
+              style={{ color: "inherit" }}
+              onClick={handleLogOut}
+            >
+              Logout
+            </span>
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <Link
+              to="/login"
+              style={{ color: "inherit", textDecoration: "none" }}
+              className="navButton"
+            >
+              Login
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
